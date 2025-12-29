ee4ed0bf60fd6f20cde4757708adecee4c17ca57
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-12-28 22:20:34 -0800

    Switch to GitHub dependencies for v0.0.1 release

--- lakefile.lean
+++ lakefile.lean
@@ -5,8 +5,8 @@ package ledger where
   version := v!"0.1.0"
 
 require batteries from git "https://github.com/leanprover-community/batteries" @ "v4.26.0"
-require crucible from ".." / "crucible"
-require staple from ".." / "staple"
+require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.1"
+require staple from git "https://github.com/nathanial/staple" @ "v0.0.1"
 
 @[default_target]
 lean_lib Ledger where
